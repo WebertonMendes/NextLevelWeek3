1bf59a1536b8917939f48e9614b75bf788365fc9
Weberton Mendes <weberton.mendes@hotmail.com>
2020-10-18 23:17:27 -0300

    Happy

--- public/js/page-orphanage.js
+++ public/js/page-orphanage.js
@@ -6,8 +6,12 @@ const options = {
     zoomControl: false
 }
 
+// get values from html
+const lat = document.querySelector('span[data-lat]').dataset.lat
+const lng = document.querySelector('span[data-lng]').dataset.lng
+
 //create map
-const map = L.map('mapid', options).setView([-18.9218962,-48.2985869], 15);
+const map = L.map('mapid', options).setView([lat,lng], 15);
 
 // create and add tileLayer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
@@ -21,7 +25,7 @@ const icon = L.icon({
 })
 
 // create and add marker
-L.marker([-18.9218962,-48.2985869], { icon })
+L.marker([lat, lng], { icon })
 .addTo(map)
 
 /* Image galery */
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const pages = require('./pages')
 const server = express()
 
 server
+.use(express.urlencoded({ extended: true }))
 .use(express.static('public'))
 
 .set('views', path.join(__dirname, "views"))
@@ -15,5 +16,6 @@ server
 .get('/orphanages', pages.orphanages)
 .get('/orphanage', pages.orphanage)
 .get('/create-orphanage', pages.createOrphanage)
+.post('/save-orphanage', pages.saveOrphanage)
 
 server.listen(5000)
